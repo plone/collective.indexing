316d542b1ac457ff97ae8a818a01085dc0f144a2
Andreas Zeidler <zeidler@jarn.com>
2008-03-31 14:33:56 +0000

    tag first alpha and bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os.path import join
 
-version = '1.0a1'
+version = open(join('collective', 'indexing', 'version.txt')).read()
 readme = open("README.txt").read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
