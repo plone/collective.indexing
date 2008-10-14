e72aa3fdc44836c86a23a16446612cf5a7343c37
Andreas Zeidler <zeidler@jarn.com>
2008-10-14 19:57:53 +0000

    copy the list so that clearing the original one doesn't affect the results

--- collective/indexing/tests/utils.py
+++ collective/indexing/tests/utils.py
@@ -49,7 +49,7 @@ class MockQueue(MockIndexer):
         pass
 
     def process(self):
-        self.processed = self.queue
+        self.processed = list(self.queue)
         self.clear()
         return len(self.processed)
 
