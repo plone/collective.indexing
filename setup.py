6402dd2f8eaf260bff26c14ab51d0791806e1ae5
Hanno Schlichting <hanno@jarn.com>
2011-07-02 12:10:01 +0200

    Start distribution metadata cleanup

--- setup.py
+++ setup.py
@@ -1,31 +1,26 @@
 from setuptools import setup, find_packages
-from os.path import join
 
-version = open(join('src', 'collective', 'indexing', 'version.txt')).read()
-readme = open("README.txt").read()
-history = open(join('docs', 'HISTORY.txt')).read()
+version = '1.9'
 
 setup(name = 'collective.indexing',
       version = version,
-      description = 'Abstract framework for queueing, optimizing and dispatching index operations for portal content.',
-      long_description = readme[readme.find('\n\n'):] + '\n' + history,
+      description = 'Abstract framework for queueing, optimizing and '
+          'dispatching index operations for Plone content.',
+      long_description = open("README.txt").read() + '\n' +
+                         open('CHANGES.txt').read(),
       classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Plone',
         'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Other Audience',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
       ],
       keywords = 'plone cmf zope indexing queueing catalog asynchronous',
       author = 'Plone Foundation',
       author_email = 'plone-developers@lists.sourceforge.net',
       url = 'http://plone.org/products/collective.indexing',
-      download_url = 'http://cheeseshop.python.org/pypi/collective.indexing/',
       license = 'GPL',
       packages = find_packages('src'),
       package_dir = {'': 'src'},
