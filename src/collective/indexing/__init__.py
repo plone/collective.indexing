31d707fef28e840bc0795fb76a762996a255d20e
Gil Forcada <gil.gnome@gmail.com>
2017-04-13 16:37:06 +0200

    Bump version on deprecation warning

--- src/collective/indexing/__init__.py
+++ src/collective/indexing/__init__.py
@@ -1,7 +1,7 @@
 import warnings
 
 warnings.warn(
-    '2.0b1 was the last release of collective.indexing. '
+    '2.0 was the last release of collective.indexing. '
     'Starting with Plone 5.1.0 its code has already merged in. '
     'For more information and upgrade information, see its PLIP: '
     'https://github.com/plone/Products.CMFPlone/issues/1343',
